b694835e80b7b92d0938580dc3953d8ef22e329f
keyt123 <tat13k0809@gmail.com>
2021-04-08 14:18:49 +0900

    tutorial 次回p112

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   public appPages = [
     {title: 'タスク登録', url: '/folder/home', icon: 'home'},
     {title: 'タスク一覧', url: '/tasks', icon: 'list'},
-    // {title: 'Inbox', url: '/folder/Inbox', icon: 'mail'},
-    // {title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane'},
-    // {title: 'Favorites', url: '/folder/Favorites', icon: 'heart'},
-    // {title: 'Archived', url: '/folder/Archived', icon: 'archive'},
-    // {title: 'Trash', url: '/folder/Trash', icon: 'trash'},
-    // {title: 'Spam', url: '/folder/Spam', icon: 'warning'},
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor() {
+  }
+  ngOnInit() {
+  }
+  ngOnDestroy() {
   }
 }
